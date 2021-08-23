f9f9d853a3b470aa356c644aaefc6522391d9e4c
nascito_nicholas <nascito.nicholas@gmail.com>
2021-08-23 14:12:29 -0300

    Inicializando projeto.

---
+++ src/main/java/br/com/fatec/sp/tcc/v1/orquestradorusers/OrquestradorUsersApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestradorusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorUsersApplication.class, args);
+	}
+
+}
